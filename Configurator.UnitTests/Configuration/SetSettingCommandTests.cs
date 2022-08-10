0ff5b29b232fe6df29522f0ef2e3f01dd3774ac3
Danny Warren <dannydwarren@gmail.com>
2022-08-09 22:14:54 -0600

    Refactored SetSettingCommand to automatically know about new settings without code changes (via reflection)
    Probably overly complicated, but a fun little project.

--- Configurator.UnitTests/Configuration/SetSettingCommandTests.cs
+++ Configurator.UnitTests/Configuration/SetSettingCommandTests.cs
@@ -10,7 +10,7 @@ namespace Configurator.UnitTests.Configuration
     public class SetSettingCommandTests : UnitTestBase<SetSettingCommand>
     {
         [Fact]
-        public async Task When_updating_settings()
+        public async Task When_updating_manifest_repo()
         {
             var settingName = "manifest.repo";
             var settingValue = new Uri($"https://{RandomString()}");
@@ -28,6 +28,52 @@ namespace Configurator.UnitTests.Configuration
                 capturedSettings.ShouldNotBeNull().ShouldSatisfyAllConditions(x =>
                 {
                     x.Manifest.Repo.ShouldBe(settingValue);
+                });
+            });
+        }
+
+        [Fact]
+        public async Task When_updating_manifest_filename()
+        {
+            var settingName = "manifest.filename";
+            var settingValue = RandomString();
+
+            GetMock<ISettingsRepository>().Setup(x => x.LoadSettingsAsync()).ReturnsAsync(new Settings());
+            
+            Settings? capturedSettings = null;
+            GetMock<ISettingsRepository>().Setup(x => x.SaveAsync(IsAny<Settings>()))
+                .Callback<Settings>(settings => capturedSettings = settings);
+            
+            await BecauseAsync(() => ClassUnderTest.ExecuteAsync(settingName, settingValue.ToString()));
+            
+            It("updates settings", () =>
+            {
+                capturedSettings.ShouldNotBeNull().ShouldSatisfyAllConditions(x =>
+                {
+                    x.Manifest.FileName.ShouldBe(settingValue);
+                });
+            });
+        }
+
+        [Fact]
+        public async Task When_updating_git_clonedirectory()
+        {
+            var settingName = "git.clonedirectory";
+            var settingValue = new Uri($@"C:\{RandomString()}");
+
+            GetMock<ISettingsRepository>().Setup(x => x.LoadSettingsAsync()).ReturnsAsync(new Settings());
+            
+            Settings? capturedSettings = null;
+            GetMock<ISettingsRepository>().Setup(x => x.SaveAsync(IsAny<Settings>()))
+                .Callback<Settings>(settings => capturedSettings = settings);
+            
+            await BecauseAsync(() => ClassUnderTest.ExecuteAsync(settingName, settingValue.ToString()));
+            
+            It("updates settings", () =>
+            {
+                capturedSettings.ShouldNotBeNull().ShouldSatisfyAllConditions(x =>
+                {
+                    x.Git.CloneDirectory.ShouldBe(settingValue);
                 });
             });
         }
--- Configurator/Configuration/SetSettingCommand.cs
+++ Configurator/Configuration/SetSettingCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Configurator.Configuration
@@ -21,12 +23,69 @@ namespace Configurator.Configuration
         {
             var settings = await settingsRepository.LoadSettingsAsync();
 
-            if (settingName == "manifest.repo")
+            if(!FindAndSet(settingName, settingValue, settings))
             {
-                settings.Manifest.Repo = new Uri(settingValue);
+                throw new ArgumentException($"{settingName} is not a recognized setting name.", "setting-name");
             }
 
             await settingsRepository.SaveAsync(settings);
+        }
+
+        private static bool FindAndSet(string settingPath, string settingValue, object parentNode, string parentPrefix = "")
+        {
+            var parentType = parentNode.GetType();
+            var parentProperties = parentType.GetProperties();
+            var leafProperties = parentProperties.Where(IsLeafProperty).ToList();
+            var nodeProperties = parentProperties.Except(leafProperties).ToList();
+
+            var setting = leafProperties.SingleOrDefault(x => BuildPropertyPath(parentPrefix, x) == settingPath);
+            if (setting != null)
+            {
+                SetValue(settingValue, parentNode, setting);
+
+                return true;
+            }
+
+            foreach (var nodeProperty in nodeProperties)
+            {
+                object? nodeValue = nodeProperty.GetValue(parentNode);
+                if (nodeValue == null)
+                    throw new Exception("We didn't expect this to happen");
+
+                var newParentPrefix = BuildPropertyPath(parentPrefix, nodeProperty);
+
+                if (FindAndSet(settingPath, settingValue, nodeValue, newParentPrefix))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void SetValue(string settingValue, object parentNode, PropertyInfo setting)
+        {
+            object typedValue = setting.PropertyType.Name switch
+            {
+                "Uri" => new Uri(settingValue),
+                _ => settingValue
+            };
+
+            setting.SetValue(parentNode, typedValue);
+        }
+
+        private static bool IsLeafProperty(PropertyInfo x)
+        {
+            return x.PropertyType.IsPrimitive
+                   || x.PropertyType == typeof(Uri)
+                   || x.PropertyType == typeof(string);
+        }
+
+        private static string BuildPropertyPath(string parentPrefix, PropertyInfo property)
+        {
+            return parentPrefix == ""
+                ? property.Name.ToLower()
+                : $"{parentPrefix}.{property.Name.ToLower()}";
         }
     }
 }
