3f29d0f41b5b43bcae961fc954103e4319f1afdd
Danny Warren <dannydwarren@gmail.com>
2022-08-13 00:07:26 -0600

    PowerShell now abstracts PowerShellRunner
    The names may need to change, but this approach is better than a single powershell result object with multiple properties that are null unless they are the desired type

---
+++ Configurator.UnitTests/PowerShell/PowerShellTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Configurator.PowerShell;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Configurator.UnitTests.PowerShell
+{
+    public class PowerShellTests : UnitTestBase<Configurator.PowerShell.PowerShell>
+    {
+        [Fact]
+        public async Task When_executing()
+        {
+            var script = RandomString();
+            var powerShellResult = new PowerShellResult
+            {
+                ExitCode = 0
+            };
+
+            GetMock<IPowerShellRunner>().Setup(x => x.ExecuteAsync(script)).ReturnsAsync(powerShellResult);
+
+            await BecauseAsync(() => ClassUnderTest.ExecuteAsync(script));
+
+            It("runs the script", () => { GetMock<IPowerShellRunner>().Verify(x => x.ExecuteAsync(script)); });
+        }
+
+        [Fact]
+        public async Task When_executing_with_unsuccessful_exit_code()
+        {
+            var script = RandomString();
+            var powerShellResult = new PowerShellResult
+            {
+                ExitCode = 1
+            };
+
+            GetMock<IPowerShellRunner>().Setup(x => x.ExecuteAsync(script)).ReturnsAsync(powerShellResult);
+
+            var exception = await BecauseThrowsAsync<Exception>(() => ClassUnderTest.ExecuteAsync(script));
+
+            It("throws", () => exception.ShouldNotBeNull());
+        }
+
+        [Fact]
+        public async Task When_executing_and_expecting_a_result_type_of_string()
+        {
+            var script = RandomString();
+            var powerShellResult = new PowerShellResult
+            {
+                ExitCode = 0,
+                LastOutput = RandomString()
+            };
+
+            GetMock<IPowerShellRunner>().Setup(x => x.ExecuteAsync(script)).ReturnsAsync(powerShellResult);
+
+            var result = await BecauseAsync(() => ClassUnderTest.ExecuteAsync<string>(script));
+
+            It("returns a typed result", () => { result.ShouldBe(powerShellResult.LastOutput); });
+        }
+
+        [Theory]
+        [InlineData("true", true)]
+        [InlineData("True", true)]
+        [InlineData("TRUE", true)]
+        [InlineData("false", false)]
+        [InlineData("False", false)]
+        [InlineData("FALSE", false)]
+        public async Task When_executing_and_expecting_a_result_type_of_bool(string lastOutput, bool expectedResult)
+        {
+            var script = RandomString();
+            var powerShellResult = new PowerShellResult
+            {
+                ExitCode = 0,
+                LastOutput = lastOutput
+            };
+
+            GetMock<IPowerShellRunner>().Setup(x => x.ExecuteAsync(script)).ReturnsAsync(powerShellResult);
+
+            var result = await BecauseAsync(() => ClassUnderTest.ExecuteAsync<bool>(script));
+
+            It("returns a typed result", () => { result.ShouldBe(expectedResult); });
+        }
+    }
+}
---
+++ Configurator/PowerShell/PowerShell.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Configurator.PowerShell
+{
+    public interface IPowerShell
+    {
+        Task ExecuteAsync(string script);
+        Task<TResult> ExecuteAsync<TResult>(string script);
+    }
+
+    public class PowerShell : IPowerShell
+    {
+        private readonly IPowerShellRunner powerShellRunner;
+
+        public PowerShell(IPowerShellRunner powerShellRunner)
+        {
+            this.powerShellRunner = powerShellRunner;
+        }
+
+        public async Task ExecuteAsync(string script)
+        {
+            await InternalExecuteAsync(script);
+        }
+
+        public async Task<TResult> ExecuteAsync<TResult>(string script)
+        {
+            var result = await InternalExecuteAsync(script);
+
+            return Map<TResult>(result.LastOutput);
+        }
+
+        private async Task<PowerShellResult> InternalExecuteAsync(string script)
+        {
+            var result = await powerShellRunner.ExecuteAsync(script);
+
+            if (result.ExitCode != 0)
+                throw new Exception($"Script failed to complete with exit code {result.ExitCode}");
+
+            return result;
+        }
+
+        private TResult Map<TResult>(string? output)
+        {
+            if (output == null)
+                return default;
+
+            var resultType = typeof(TResult);
+            object objResult = resultType switch
+            {
+                {} when resultType == typeof(string) => output,
+                {} when resultType == typeof(bool) => bool.Parse(output),
+                _ => throw new NotSupportedException($"PowerShell result type of '{typeof(TResult).FullName}' is not yet supported")
+            };
+
+            return (TResult)objResult;
+        }
+    }
+}
