9f0a24c2b6716a8fa7448b68ddb55824718c0536
Danny Warren <dannydwarren@gmail.com>
2022-08-12 22:30:22 -0600

    PowerShell (v2) runs scripts as admin and returns single output

---
+++ Configurator.IntegrationTests/PowerShell/PowerShellTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Configurator.IntegrationTests.PowerShell
+{
+    public class PowerShellTests : IntegrationTestBase<Configurator.PowerShell.PowerShell>
+    {
+        [Fact]
+        public async Task When_executing()
+        {
+            var script = @"Write-Host 'Hello World'";
+
+            var result = await BecauseAsync(() => ClassUnderTest.ExecuteAsync(script));
+
+            It("runs with a successful exit code", () =>
+            {
+                result.ExitCode.ShouldBe(0);
+            });
+        }
+        
+        [Fact]
+        public async Task When_executing_and_expecting_result()
+        {
+            var expectedOutput = RandomString();
+            
+            var script = $@"Write-Output '{expectedOutput}'";
+
+            var result = await BecauseAsync(() => ClassUnderTest.ExecuteAsync(script));
+
+            It("returns the script output", () =>
+            {
+                result.Output.ShouldNotBeNull().ShouldBe(expectedOutput);
+            });
+        }
+    }
+}
---
+++ Configurator/PowerShell/PowerShell.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Configurator.PowerShell
+{
+    public interface IPowerShell
+    {
+        Task<PowerShellResult> ExecuteAsync(string script);
+    }
+
+    public class PowerShell : IPowerShell
+    {
+        public async Task<PowerShellResult> ExecuteAsync(string script)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    Verb = "runas",
+                    FileName = @"pwsh.exe",
+                    Arguments = @$"-Command ""{script}"""
+                },
+                EnableRaisingEvents = true
+            };
+            
+            var result = new PowerShellResult();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Token.Register(() =>
+            {
+                result.ExitCode = process.ExitCode;
+                process.Dispose();
+            });
+
+            process.Start();
+
+            var outputLoop = Task.Run(() =>
+            {
+                while (!process.StandardOutput.EndOfStream && !process.HasExited)
+                {
+                    result.Output = process.StandardOutput.ReadLine();
+                }
+            }, cancellationTokenSource.Token);
+
+            var exitLoop = Task.Run(() =>
+            {
+                process.WaitForExit();
+            }, cancellationTokenSource.Token);
+
+            await exitLoop;
+            
+            cancellationTokenSource.Cancel();
+
+            await outputLoop;
+
+            return result;
+        }
+    }
+
+    public class PowerShellResult
+    {
+        public int ExitCode { get; set; } = -1;
+        public string? Output { get; set; }
+    }
+}
