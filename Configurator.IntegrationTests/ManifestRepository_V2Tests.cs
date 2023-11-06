0727d5eefc4b479c088afbdd1973c4cd4debffe6
Danny Warren <dannydwarren@gmail.com>
2023-11-06 09:11:26 -0700

    Add all existing WingetApp integration tests to ManifestRepository_V2

--- Configurator.IntegrationTests/ManifestRepository_V2Tests.cs
+++ Configurator.IntegrationTests/ManifestRepository_V2Tests.cs
@@ -12,7 +12,7 @@ namespace Configurator.IntegrationTests
         {
             var manifest = await BecauseAsync(() => ClassUnderTest.LoadAsync());
 
-            It("loads basic winget app", () =>
+            It($"loads basic {nameof(WingetApp)}", () =>
             {
                 manifest.Apps[0]
                     .ShouldBeOfType<WingetApp>().ShouldSatisfyAllConditions(x =>
@@ -23,6 +23,43 @@ namespace Configurator.IntegrationTests
                         x.Configuration.ShouldBeNull();
                     });
             });
+
+            It($"loads {nameof(WingetApp)} with {nameof(WingetApp.InstallArgs)}", () =>
+            {
+                manifest.Apps[1]
+                    .ShouldBeOfType<WingetApp>().ShouldSatisfyAllConditions(x =>
+                    {
+                        x.AppId.ShouldBe("winget-app-id-with-install-args");
+                        x.InstallArgs.ShouldBe(" --override install-args");
+                    });
+            });
+
+            It($"loads {nameof(WingetApp)} with {nameof(WingetApp.PreventUpgrade)}", () =>
+            {
+                manifest.Apps[2]
+                    .ShouldBeOfType<WingetApp>().ShouldSatisfyAllConditions(x =>
+                    {
+                        x.AppId.ShouldBe("winget-app-id-with-prevent-upgrade");
+                        x.PreventUpgrade.ShouldBeTrue();
+                    });
+            });
+
+            It($"loads {nameof(WingetApp)} with {nameof(WingetApp.Configuration)}", () =>
+            {
+                manifest.Apps[3]
+                    .ShouldBeOfType<WingetApp>().ShouldSatisfyAllConditions(x =>
+                    {
+                        x.AppId.ShouldBe("winget-app-id-with-configuration");
+                        x.Configuration.RegistrySettings.ShouldHaveSingleItem()
+                            .ShouldSatisfyAllConditions(y =>
+                            {
+                                y.KeyName.ShouldBe("key-name-test");
+                                y.ValueName.ShouldBe("value-name-test");
+                                y.ValueData.ShouldBe("value-data-test");
+                            });
+                    });
+            });
+
         }
     }
 }
